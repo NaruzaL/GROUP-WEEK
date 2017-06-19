86a6b61a7528823ab1788b7c79b4e638472f0e4a
Pete Lazuran <pdlazuran@gmail.com>
2017-06-18 19:34:56 -0700

    clarify thoughts in notes of HomeModule

--- Modules/HomeModuel.cs
+++ Modules/HomeModuel.cs
@@ -15,10 +15,10 @@ namespace PersonaFive
 
       Get["/first_question/ask"] = _ => {
         Dictionary<string, object> model = new Dictionary<string, object>{};
-        List<Shadow> allShadows = Shadow.GetAll(); //randomly pick by RNG
-        Shadow randomShadow = allShadows.RandomMethod;
-        List<Answer> shadowAnswers = Shadow.GetAnswers();
-        //make random selecion of one answer in shadowAnswers, shadowAnswer = shadowAnswers[i] (probably no method required)
+        List<Shadow> allShadows = Shadow.GetAll(); //randomly pick by RNG "int i = whatever random math is"
+        Shadow randomShadow = allShadows[i];
+        List<Answer> shadowAnswer = randomShadow.GetAnswers();
+        //make random selecion of one answer in shadowAnswers, shadowAnswer = shadowAnswers[i] (same as randomShadow)
         Question randomQuestion = shadowAnwer.FindQuestion(); //FindQuestion will need to be written
         List<Answer> allAnswers = Answers.GetAll();
         //make random selection of two other answers that are not same type as selected shadow (method ?AddDummies?), List<Answer> answers = ..., answer.Add("shadowanswer"), shuffle positions of list then display(method ?Shuffle?) ["anwers", answers]
@@ -26,12 +26,13 @@ namespace PersonaFive
         model.Add("answers", answers);
         model.Add("quesion", randomQuestion);
         return View["first_question.cshtml", model];
-      }
+      };
 
       Post["/first_question/result/{id}"] = parameters => {
         Dictionary<string, object> model = new Dictionary<string, object>{};
         Shadow randomShadow = new Shadow.Find(parameters.id);
         List<Answer> shadowAnswers = Shadow.GetAnswers();
+        string result = "";
         if (shadowAnswers.Contains(Request.Form["user-choice"]))
         {
           result = "good";
@@ -45,12 +46,12 @@ namespace PersonaFive
         // in .cshtml have "next question" link appear if result == good <a href = "/second_question/ask/@Model["shadow"].GetId()">Next question</a>
         // otherwise link back to "/"
         return View["result.cshtml", model];
-      }
+      };
 
       Get["second_question/ask/{id}"] = parameters => {
         Shadow sameShadow = new Shadow.Find(parameters.id);
         //do random question again same way as before
-      }
+      };
 
     }
   }
