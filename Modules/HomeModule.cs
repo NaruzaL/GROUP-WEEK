7c5f1cf94480d8c042c9889ca0701467ef240dcd
Pete Lazuran <pdlazuran@gmail.com>
2017-06-19 20:44:14 -0700

    add all routes in homemodule

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -17,13 +17,13 @@ namespace PersonaFive
         Dictionary<string, object> model = new Dictionary<string, object>{};
         List<Shadow> allShadows = Shadow.GetAll();
         int i = new Random().Next(1, allShadows.Count + 1 );
-        Shadow randomShadow = allShadows[i-1];//randomly chosen shadow
+        Shadow randomShadow = allShadows[i-1];
 
         List<Answer> shadowAnswers = randomShadow.GetAnswers();
 
         List<Question> answerQuestions = shadowAnswers[0].GetQuestions();
         int j = new Random().Next(1, answerQuestions.Count + 1);
-        Question newQuestion = answerQuestions[j-1];//Question for the answers
+        Question newQuestion = answerQuestions[j-1];
 
         List<Answer> newAnswers = newQuestion.GetAnswers();
 
@@ -34,30 +34,22 @@ namespace PersonaFive
       };
 
       Post["/first_question/result/{id}"] = parameters => {
+        Dictionary<string,object> model = new Dictionary<string,object>{};
         Shadow randomShadow = Shadow.Find(Request.Form["shadow-id"]);
         Answer selectedAnswer = Answer.Find(Request.Form["answer-id"]);
-        // put if statement into cshtmlpage
-        if (selectedAnswer.GetAnswerType() == randomShadow.GetShadowType())
-        {
-          string result = "Your answer pleases me";
-        }
-        else
-        {
-          string result = "DIE!";
-        }
-        // in .cshtml have "next question" link appear if result == good <a href = "/second_question/ask/@Model["shadow"].GetId()">Next question</a>
-        // otherwise link back to "/"
-        return View["result.cshtml", randomShadow];
+        model.Add("shadow", randomShadow);
+        model.Add("answer", selectedAnswer);
+        return View["result.cshtml", model];
       };
 
       Get["second_question/ask/{id}"] = parameters => {
         Dictionary<string, object> model = new Dictionary<string, object>{};
         Shadow sameShadow = Shadow.Find(Request.Form["shadow-id"]);
-        List<Answer> shadowAnswers = sameShadow.GetAnswers();//answers that will be linked to it in DB
 
+        List<Answer> shadowAnswers = sameShadow.GetAnswers();
         List<Question> answerQuestions = shadowAnswers[0].GetQuestions();
         int j = new Random().Next(1, answerQuestions.Count + 1);
-        Question newQuestion = answerQuestions[j-1];//Question for the answers
+        Question newQuestion = answerQuestions[j-1];
 
         List<Answer> newAnswers = newQuestion.GetAnswers();
 
@@ -65,6 +57,20 @@ namespace PersonaFive
         model.Add("answers", newAnswers);
         model.Add("question", newQuestion);
         return View["first_question.cshtml", model];
+      };
+
+      Post["/second_question/result/{id}"] = parameters => {
+        Dictionary<string,object> model = new Dictionary<string,object>{};
+        Shadow randomShadow = Shadow.Find(Request.Form["shadow-id"]);
+        Answer selectedAnswer = Answer.Find(Request.Form["answer-id"]);
+        model.Add("shadow", randomShadow);
+        model.Add("answer", selectedAnswer);
+        return View["second_result.cshtml", model];
+      };
+
+      Get["/capture/{id}"] = parameters => {
+        Shadow capturedShadow = Shadow.Find(Request.Form["shadow-id"]);
+        return View["capture.cshtml"];
       };
 
     }
