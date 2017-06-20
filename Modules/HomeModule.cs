b4b10e90dd7b49d330784d6588c4b3835ec617c2
Pete Lazuran <pdlazuran@gmail.com>
2017-06-19 20:10:31 -0700

    add fixes to homemodule, should work

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -19,30 +19,31 @@ namespace PersonaFive
         int i = new Random().Next(1, allShadows.Count + 1 );
         Shadow randomShadow = allShadows[i-1];//randomly chosen shadow
 
-        List<Answer> shadowAnswers = randomShadow.GetAnswers();//answers that will be linked to it in DB
+        List<Answer> shadowAnswers = randomShadow.GetAnswers();
 
         List<Question> answerQuestions = shadowAnswers[0].GetQuestions();
         int j = new Random().Next(1, answerQuestions.Count + 1);
-        Question newQuestion = answerQuestions[j-1]//Question for the answers
+        Question newQuestion = answerQuestions[j-1];//Question for the answers
+
+        List<Answer> newAnswers = newQuestion.GetAnswers();
 
         model.Add("shadow", randomShadow);
-        model.Add("answers", shadowAnswers);
+        model.Add("answers", newAnswers);
         model.Add("question", newQuestion);
         return View["first_question.cshtml", model];
       };
 
       Post["/first_question/result/{id}"] = parameters => {
-
-        Shadow randomShadow = new Shadow.Find(Request.Form["shadow-id"]);
-        Answer selecetedAnswer = Answer.Find(Request.Form["answer-id"]);
+        Shadow randomShadow = Shadow.Find(Request.Form["shadow-id"]);
+        Answer selectedAnswer = Answer.Find(Request.Form["answer-id"]);
         // put if statement into cshtmlpage
         if (selectedAnswer.GetAnswerType() == randomShadow.GetShadowType())
         {
-          result = "Your answer pleases me";
+          string result = "Your answer pleases me";
         }
         else
         {
-          result = "DIE!";
+          string result = "DIE!";
         }
         // in .cshtml have "next question" link appear if result == good <a href = "/second_question/ask/@Model["shadow"].GetId()">Next question</a>
         // otherwise link back to "/"
@@ -51,15 +52,17 @@ namespace PersonaFive
 
       Get["second_question/ask/{id}"] = parameters => {
         Dictionary<string, object> model = new Dictionary<string, object>{};
-        Shadow sameShadow = new Shadow.Find(Request.Form["shadow-id"]);
-        List<Answer> shadowAnswers = randomShadow.GetAnswers();//answers that will be linked to it in DB
+        Shadow sameShadow = Shadow.Find(Request.Form["shadow-id"]);
+        List<Answer> shadowAnswers = sameShadow.GetAnswers();//answers that will be linked to it in DB
 
         List<Question> answerQuestions = shadowAnswers[0].GetQuestions();
         int j = new Random().Next(1, answerQuestions.Count + 1);
-        Question newQuestion = answerQuestions[j-1]//Question for the answers
+        Question newQuestion = answerQuestions[j-1];//Question for the answers
+
+        List<Answer> newAnswers = newQuestion.GetAnswers();
 
         model.Add("shadow", sameShadow);
-        model.Add("answers", shadowAnswers);
+        model.Add("answers", newAnswers);
         model.Add("question", newQuestion);
         return View["first_question.cshtml", model];
       };
